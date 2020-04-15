33a9b964ca06514a7f241d1f4b4c9c60f9b8d0d0
Toqui012 <alvaroseba2000@gmail.com>
2020-04-15 02:15:14 -0400

    Css Implementado en proyecto base

--- mapi_website/mapi_website/settings.py
+++ mapi_website/mapi_website/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mapi_app'
+    'mapi_app',
+    'sass'
 ]
 
 MIDDLEWARE = [
