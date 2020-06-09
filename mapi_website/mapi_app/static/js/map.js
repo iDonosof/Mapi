02be983dfa974d46ba22997745dec03381098cbb
Samuel <samuelpguzman@hotmail.com>
2020-06-08 22:23:28 -0400

    Agrega listados y detalle de evento

--- mapi_website/mapi_app/static/js/map.js
+++ mapi_website/mapi_app/static/js/map.js
@@ -1,4 +1,4 @@
-var map, directionsService, directionsRenderer, miubicacion, htmlGoogle, nearEvents = [];
+var map, directionsService, directionsRenderer, miubicacion, htmlGoogle, nearEvents = [], groupMarkers = [];
 
 var chileBounds = {
   north: -17.2,
@@ -21,11 +21,6 @@ function initMap() {
     },
     disableDefaultUI: true,
     zoomControl: true,
-    mapTypeControl: true,
-    mapTypeControlOptions: {
-      mapTypeIds: ['styleMap', 'satellite'],
-      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-    },
     streetViewControl: true,
   });
 
@@ -42,6 +37,7 @@ function initMap() {
   map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(openEventsDiv);
 
   var searchControlDiv = document.createElement('div');
+  var mapdiv = new mapTypeControl(searchControlDiv, map);
   searchControlDiv.id = 'searchBarControl';
   searchControlDiv.className = 'col-sm-8';
   var srcControl = searchBarControl(searchControlDiv, map);
@@ -71,6 +67,51 @@ function obtainGpsControl(controlDiv, map) {
     }else
       alert("Por favor activa el gps o acepta el permiso para acceder a esta funcionalidad")
   });
+}
+
+function mapTypeControl(controlDiv, map){
+  var typeButton = document.createElement('button');
+  typeButton.className = 'dropbtn';
+  typeButton.title = 'Seleciona la vista del mapa';
+  typeButton.innerHTML = 'Tipo de mapa';
+  controlDiv.appendChild(typeButton);
+
+  var typeDiv = document.createElement('div');
+  typeDiv.className = 'dropdown-content';
+  typeDiv.id = 'myDropdown';
+  typeButton.appendChild(typeDiv);
+
+  var mapiA = document.createElement('a');
+  mapiA.innerHTML = 'Mapi Map';
+  typeDiv.appendChild(mapiA);
+
+  var satelliteiA = document.createElement('a');
+  satelliteiA.innerHTML = 'Satelital';
+  typeDiv.appendChild(satelliteiA);
+
+  typeButton.addEventListener('click', function() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  });
+  mapiA.addEventListener('click', function() {
+    map.setMapTypeId('styleMap');
+  });
+  satelliteiA.addEventListener('click', function() {
+    map.setMapTypeId('satellite');
+  });
+}
+
+//Funcion que cierra el Drop menu del tipo al hacer click en la pantalla
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
 
 function searchBarControl(controlDiv) {
@@ -157,7 +198,6 @@ function rangoGps(){
 
 // Funcion que trae las cordenadas desde la api y las coloca en el mapa, junto con la animacion bounce y se le agrega una funcion click para abrir tarjeta
 function addEvents(){
-  var groupMarkers = [];
   var markerEvents;
   fetch('http://127.0.0.1:8000/api/events/list')
   .then( res => res.json())
@@ -170,14 +210,18 @@ function addEvents(){
       markerEvents = new google.maps.Marker({
           id: events[i].id,
           position: LatLng[0],
-          title:"Mi posicion",
-          map: map 
+          title: events[i].name,
+          map: map,
+          type: events[i].table
       });
+      markerEvents.customInfo = events[i].id+","+events[i].table +","+ events[i].latitude+","+ events[i].longitude;
 
-      markerEvents.addListener('click', function () {
-        openPanel();
+      google.maps.event.addListener(markerEvents, 'click', function () {
+        openPanel(this.customInfo);
       });
       groupMarkers.push(markerEvents)
+
+
     }
 
     var markerCluster = new MarkerClusterer(map, groupMarkers,
@@ -187,27 +231,49 @@ function addEvents(){
 
 
 // Funcion que abre y cierra la tarjeta de evento al hacerle click
-function openPanel(){
-  document.getElementById("menuPanel").style.width="100%";
+function openPanel(customInfo){
+  document.getElementById("menuPanel").style.width="50%";
+  document.getElementById("map").style.width="50%";
+  document.getElementById("map").style.marginLeft="50%";
+  document.getElementById("map").style.transition= "all 1s";
+  showDetailEvent(customInfo)
 }
 function closePanel(){
   document.getElementById("menuPanel").style.width="0%";
+  document.getElementById("map").style.width="100%";
+  document.getElementById("map").style.marginLeft="0";
+  document.getElementById("map").style.transition= "all 1s";
+  setTimeout(function(){
+    detailContent = document.querySelector(".panelContent");
+    detail = detailContent.querySelector("#detailEvent");
+    detailContent.removeChild(detail);
+  }, 1000); 
 }
 
 // Funcion que abre y cierra la lista de eventos cercanos o todos al hacerle click
 function openPanelR(){
+  var detailContent = document.querySelector(".panelContent");
+  var detail = detailContent.querySelector("#detailEvent");
+  document.getElementById("menuPanel").style.width="0%";
+  document.getElementById("map").style.width="100%";
+  document.getElementById("map").style.marginLeft="0";
   document.getElementById("menuPanelR").style.width="100%";
-  if(nearEvents[1]==undefined){
-    showAllEvents();
+  if(nearEvents[0]==undefined){
+    document.getElementById("neartEventsBtn").disabled=true;
   }
-  else{
-    showNearEvents();
+  else document.getElementById("neartEventsBtn").disabled=false;
+  showAllEvents();
+  if(detail!==null){
+    setTimeout(function(){
+      detailContent.removeChild(detail);
+    }, 1000);
   }
 }
+
 function closePanelR(){
   document.getElementById("menuPanelR").style.width="0%";
   setTimeout(function(){
-    card = document.querySelector(".panelContentR");
+    card = document.querySelector(".eventListTable");
     card.querySelectorAll('*').forEach(n => n.remove());
   }, 1000); 
 
@@ -308,59 +374,172 @@ function calcRoute(i) {
 
       const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
       const d = R * c;
-      console.log(d);
-      if(d < 2.6){
+      if(d < 1.6){
         nearEvents.push(events[i]);
       }
     }
   })
-  
-  
+}
+
+//Funcion que muestra el detalle del evento al hacerle click
+function showDetailEvent(customInfo){
+  detail = document.querySelector(".panelContent")
+  var id = customInfo.split(',')[0];
+  var typeEvent = customInfo.split(',')[1];
+
+  fetch('http://127.0.0.1:8000/api/events/detail/'+typeEvent+'/'+id)
+  .then( res => res.json())
+  .then(events => {
+    detailEvent = document.createElement('div');
+    detailEvent.id = "detailEvent";
+    detailEvent.innerHTML = 
+      `<h1>${events.name}</h1>
+      <img src='/media/workshops/2020/06/08/curso-online-pasteleria-reposteria_amp_primaria_1_1560502963.jpg' alt='caca'>
+      <b><p>Comuna:</b> ${events.commune}</p>
+      <b><p>Tipo de evento:</b> ${typeEvent} - ${events.type}</p>
+      <b><p>Descripcion:</b>  ${events.description}</p>
+      <b><p>Direccion:</b>  ${events.address}</p>
+      <b><p>Inicio:</b>  ${events.start_date}, ${events.start_time}</p>
+      <b><p>Termino:</b>  ${events.ended_date}, ${events.ended_time}</p>
+      <b><p>Puntuaciones:</b></p>
+      <p><img id="starsImg" src="static/img/5stars.png" alt="5 estrellas imagen"> = <b>${events.five_stars}</b></p>
+      <p><img id="starsImg" src="static/img/4stars.png" alt="4 estrellas imagen"> = <b>${events.four_stars}</b></p>
+      <p><img id="starsImg" src="static/img/3stars.png" alt="3 estrellas imagen"> = <b>${events.three_stars}</b></p>
+      <p><img id="starsImg" src="static/img/2stars.png" alt="2 estrellas imagen"> = <b>${events.two_stars}</b></p>
+      <p><img id="starsImg" src="static/img/1stars.png" alt="1 estrellas imagen"> = <b>${events.one_stars}</b></p>
+      <h1>Comentarios de la gente</h1>
+      <p>${events.comments}</p>`;
+
+    detail.appendChild(detailEvent);
+   })
 }
 
 //Funcion que añade los eventos cercanos a la lista
 function showNearEvents(){
 
-  card = document.querySelector(".panelContentR");
+  card = document.querySelector(".eventListTable");
   for (var i = 0; i < nearEvents.length; i++) {
-    listNear = document.createElement('div');
+    listNear = document.createElement('li');
     listNear.id = "listEvents";
-    listNear.innerHTML = "<h1>"+nearEvents[i].name+"</h1><img src='static/img/futbol.jpg' alt='caca'>";
+    listNear.innerHTML = "<h1>"+nearEvents[i].name+"</h1><img src='static/img/futbol.jpg' alt='caca'><p>Tipo de evento:"+events[i].table+"</p>";
     card.appendChild(listNear);
   }
 }
 
 //Funcion que trae todos los eventos para mostrar
-function showAllEvents(){
+async function showAllEvents(){
 
-  card = document.querySelector(".panelContentR");
-  
-  fetch('http://127.0.0.1:8000/api/events/list')
-  .then( res => res.json())
-  .then(events => {
-    for (var i = 0; i < events.length; i++) {  
-      listAll = document.createElement('div');
+  card = document.querySelector(".eventListTable");
+
+  for (var i = 0; i < groupMarkers.length; i++){
+    var id = groupMarkers.id;
+    var caca = groupMarkers[i];
+
+    await fetch('http://127.0.0.1:8000/api/events/detail/'+groupMarkers[i].type+'/'+groupMarkers[i].id)
+    .then( res => res.json())
+    .then(events => {
+      listAll = document.createElement('li');
       listAll.id = "listEvents";
-      listAll.innerHTML = "<h1>"+events[i].name+"</h1><img src='static/img/futbol.jpg' alt='caca'>";
-      card.appendChild(listAll);
-    }
-  })
+      listAll.innerHTML = 
+        `<h1 onclick="centerEventClick(${events.longitude}, ${events.latitude})">${events.name}</h1>
+        <img src='/media/workshops/2020/06/08/curso-online-pasteleria-reposteria_amp_primaria_1_1560502963.jpg' alt='caca'>
+        <b><p>Comuna:</b> ${events.commune}</p>
+        <b><p>Categoria de evento:</b>${events.type}</p>
+        <b><p>Tipo de evento:</b>${groupMarkers[i].type}</p>
+        <b><p>Descripcion:</b>  ${events.description}</p>
+        <b><p>Inicio:</b>  ${events.start_date}, ${events.start_time}</p>
+        <b><p>Termino:</b>  ${events.ended_date}, ${events.ended_time}</p>`;
+      card.appendChild(listAll);  
+    })    
+  }
 }
 
-// Funcion que filtra los eventos en la lista
-function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
+
+//Funcion que al clickear el titulo del evento lo centra en el mapa
+function centerEventClick(lng, lat){
+  closePanelR()
+  LatLng = [{lat: parseFloat(lat), lng: parseFloat(lng)}]
+  latE = lat;
+  lngE = lng;
+
+  map.setCenter(LatLng[0]);
+  map.setZoom(18);
+
+  for (i = 0; i < groupMarkers.length; i++){
+    
+    if(latE===parseFloat(groupMarkers[i].customInfo.split(',')[2]) & lngE===parseFloat(groupMarkers[i].customInfo.split(',')[3]) ){
+      groupMarkers[i].setAnimation(google.maps.Animation.BOUNCE);
+      eventCircle = new google.maps.Circle({
+        strokeWeight: 0,
+        fillColor: 'red',
+        fillOpacity: 0.35,
+        map: map,
+        center: {lat: parseFloat(groupMarkers[i].customInfo.split(',')[2]), lng: parseFloat(groupMarkers[i].customInfo.split(',')[3])},
+        radius: 40
+      });  
+      resetMarker(groupMarkers, i, eventCircle)
+    }
+  }
+}
+
+//Funcion que quita la animacion y circulo en 4 segundos
+function resetMarker(mark, i, eventCircle){
+  setTimeout(() => {      
+    eventCircle.setMap(null);
+    mark[i].setAnimation(null); 
+  }, 4000);
+}
+
+// Funcion que filtra los eventos en la lista por nombre
+
+function nameFilterEvent() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("filterInputName");
+  filter = input.value.toUpperCase();
+
+  ul = document.getElementsByClassName("eventListTable");
+  li = ul[0].getElementsByTagName("li");
+  
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("h1")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+  }
+}
+
+
+// Funciones para filtar los eventos por tipo de evento
+
+function buttonFilterEvent(type) {
+  
+  var filter, ul, li, i, txtValue, txtValue1, txtValue2;
+  filter = type.toUpperCase();
+  input = document.getElementById("filterInputName");
+  input.value = '';
+  ul = document.getElementsByClassName("eventListTable");
+  li = ul[0].getElementsByTagName("li");
+
     for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
+      p = li[i].getElementsByTagName("p")[1];
+    
+
+      txtValue = p.textContent.split(':')[1];
+      txtValue1 = txtValue.split('-')[0];
+      // txtValue2 = txtValue.split('-')[1];
+
+      if (txtValue1.toUpperCase().indexOf(filter) > -1)  {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+      if (filter=="ALL"){
+        li[i].style.display = "";
+      }
+      if(nearEvents[1]==undefined & filter=="NEAR"){
+      }
     }
 }
